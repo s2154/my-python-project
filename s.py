7252dfed8470183b2f1b52a4c688fc4d81dbb218
s2154 <129718193+s2154@users.noreply.github.com>
2023-04-03 11:26:46 +0530

    Add files via upload

---
+++ s.py
@@ -0,0 +1,11 @@
+x="what a beautiful day!"
+#positive indexing
+#negaetaive indexing
+print(x[:])
+print(x[3:8])
+print(x[2:10])
+print(x[8:16])
+print(x[18:])
+print(x[15:20])
+print(x[2:10])
+print(x[:8])
