e113256741e9eebaca0e0d672c6c46fdbe1da204
s2154 <129718193+s2154@users.noreply.github.com>
2023-04-03 11:22:41 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,24 @@
+a=15
+b=10
+
+#single line if
+if a>10:
+           print ("a is grater than 10")
+           
+
+
+#if...else
+if a>b:                     
+             print("a is greater than b")
+else:
+             print("b is greater than a")
+
+#ladder
+
+if a>b:
+            print("a is greater than b")
+elif b>a:
+            print( " b is greater than a")
+else:
+            print("both are equal")
+            
